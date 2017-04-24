56f5d93b47d534c84766e6121717ed1465afc86c
Ari Ayvazyan <ariay@live.at>
2017-04-24 17:35:28 +0200

    Update GitConfiguration.sh

--- GitConfiguration.sh
+++ GitConfiguration.sh
@@ -1,2 +1,5 @@
-#git lg (pretty log)
+#git lg //pretty log
 git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+
+#git cleanup //Deletes all local repos except master, develop and the current branch
+git config alias.cleanup "!git branch --merged | grep  -v '\\*\\|master\\|develop' | xargs -n 1 git branch -d"
