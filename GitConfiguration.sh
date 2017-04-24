a60bd962485d1cf3d48ff907db4b6ea99a155bfd
Ari Ayvazyan <ariay@live.at>
2017-04-24 11:24:03 +0200

    Create GitConfiguration.sh

---
+++ GitConfiguration.sh
@@ -0,0 +1,2 @@
+#git lg (pretty log)
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
