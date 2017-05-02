c1dfac4082ae37b5e49fd6dc3a35eae2e35a3bd7
Ari Ayvazyan <ariay@live.at>
2017-05-02 15:12:14 +0200

    Update GitConfiguration.sh

--- GitConfiguration.sh
+++ GitConfiguration.sh
@@ -1,3 +1,5 @@
+#Execute with git shell on windows
+
 #git lg //pretty log
 git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 
@@ -7,5 +9,9 @@ git config alias.cleanup "!git branch --merged | grep  -v '\\*\\|master\\|develo
 #git amend //amend changes to the last commit without editing the message
 git config --global alias.amend "commit --amend --no-edit"
 
-#git lg //pretty log
+#git pf //push force alias
 git config --global alias.pf "push --force"
+
+#git sb //switch branch by entering partial name
+git config --global alias.sco '!sh -c "git branch -a | grep -v remotes | grep $1 | xargs git checkout"'
+
