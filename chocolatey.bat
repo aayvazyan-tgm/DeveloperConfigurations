f793da16900b7a1d096e9b1024cecc85db1b4992
Ari Ayvazyan <ariay@live.at>
2019-09-10 10:11:54 +0200

    Create chocolatey.bat

---
+++ chocolatey.bat
@@ -0,0 +1,19 @@
+choco install git
+choco install nodejs
+choco install notepadplusplus
+choco install intellijidea-ultimate
+choco install androidstudio
+
+choco install postman
+choco install winscp
+choco install putty
+choco install 7zip
+
+choco install keepass
+choco install keepass-plugin-keeanywhere
+choco install telegram
+choco install franz
+
+choco install vlc
+choco install googlechrome
+
